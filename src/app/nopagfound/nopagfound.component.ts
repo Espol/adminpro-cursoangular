6bf39f568a3b2c9a0a2bde726d58c7bde22ec2ae
mmoyano <mmoyanol1180@gmail.com>
2018-12-21 23:15:11 -0500

    Primer commit

---
+++ src/app/nopagfound/nopagfound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagfound',
+  templateUrl: './nopagfound.component.html',
+  styles: []
+})
+export class NopagfoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
