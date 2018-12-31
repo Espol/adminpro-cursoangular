4ef5a1494b59e9694cee093e8cc891232d0aaaf0
mmoyano <mmoyanol1180@gmail.com>
2018-12-31 15:19:27 -0500

    Input, output, viewchild

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progreso1: number = 20;
+  progreso2: number = 50;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  actualizar( event: number) {
+    this.progreso1 = event;
+  }
+
+  /* cambiarValor( valor: number ) {
+    if(this.progreso <= 0  && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+    if(this.progreso >= 100 && valor > 0) {
+      this.progreso = 100;
+      return;
+    }
+    this.progreso = this.progreso + valor;
+  } */
 
 }
